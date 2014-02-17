fe18c7822b00fd9dd3cb6fce8cfe2835a235e996
taka <taka@idancoffee.com>
2014-02-17 14:12:53 +0800

    add second comment

--- .emacs.d/config/set-auto-async-byte-compile.el
+++ .emacs.d/config/set-auto-async-byte-compile.el
@@ -1,4 +1,5 @@
 ;; auto-async-byte-compile
+;; add second comment
 (add-to-list 'load-path "~/.emacs.d/elisp/auto-async-byte-compile")
 (require 'auto-async-byte-compile)
 (setq auto-async-byte-compile-exclude-files-regexp "/junc/")
